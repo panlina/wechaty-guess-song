dc2563928c5b7b03b5013f126fc8d906064acd06
Jack Lu <jacklu@jacklu.me>
2023-02-01 10:42:43 +0800

    start game by room members sending command

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 /** @typedef { import("wechaty").Wechaty } Wechaty */
 /** @typedef { import("wechaty").Room } Room */
 /** @typedef { import("wechaty").Message } Message */
+/** @typedef { import("wechaty-puppet").RoomQueryFilter } RoomQueryFilter */
 var { MessageType, FileBox } = require("wechaty-puppet");
 
 /**
  * @param {Object} config
+ * @param {RoomQueryFilter[]} [config.filter] the filter of rooms to enable this plugin
  * @param {() => Promise<{ title: string, preview: FileBox }>} config.fetch song fetcher
  * @param {number} config.voteMin min vote to start the game
  * @param {number} config.voteTimeout vote timeout in milliseconds
@@ -17,18 +19,23 @@ module.exports = function WechatyGuessSongPlugin(config) {
 			bot.off("message", listener);
 		};
 		async function listener(/** @type {Message} */message) {
-			if (message.talker().self()) {
-				var match = message.text().match(/^guess-song: (.*)/);
-				if (match) {
-					var [, roomTopic] = match;
-					var room = await bot.Room.find({ topic: roomTopic });
-					if (!room) {
-						bot.userSelf().say(`Room ${roomTopic} not found`);
-						return;
-					}
-					play(room);
-				}
-			}
+			var room = message.room();
+			if (
+				room
+				&&
+				message.text() == "猜歌曲" && (
+					!config.filter || (
+						await Promise.all(
+							config.filter.map(
+								async filter => bot.puppet.roomQueryFilterFactory(filter)(
+									await bot.puppet.roomPayload(room.id)
+								)
+							)
+						)
+					).some(Boolean)
+				)
+			)
+				play(room);
 		}
 	};
 	async function play(/** @type {Room} */room) {
